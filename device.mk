0d1a4ac2d1ad3eba104d5bd08021f1977231d594
Jamie Falconer <mrfluffyoven@gmail.com>
2024-10-10 11:26:09 +0000

    add libspl

--- device.mk
+++ device.mk
@@ -25,6 +25,7 @@ TARGET_RECOVERY_DEVICE_MODULES += \
     libdmabufheap \
     libhidlmemory \
     libion \
+    libspl \
     libnetutils \
     libxml2 \
     vendor.display.config@1.0 \
@@ -37,6 +38,7 @@ RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libdmabufheap.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libhidlmemory.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libspl.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libnetutils.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
